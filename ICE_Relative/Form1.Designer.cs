eeea86d5a7355a0b06c698e7907826d8308ae766
MmAaXx500 <viktor.balogh2000@gmail.com>
2019-02-08 19:36:04 +0100

    Add project files.

---
+++ ICE_Relative/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ICE_Relative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 226);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::ICE_Relative.Properties.Resources.DragAndDrop;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "ICE_Relative";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.DDEnd);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.DDStart);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ICE_Relative/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml;
+using System.Threading;
+
+namespace ICE_Relative
+{
+    public partial class Form1 : Form
+    {
+
+        public bool processing = false;
+        public string[] FileList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker1.DoWork += new DoWorkEventHandler(BackgroundWorker1_DoWork);
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker1_RunWorkerCompleted);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(BackgroundWorker1_ProgressChanged);
+        }
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            FixTheFiles(worker, e);
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            processing = false;
+            pictureBox1.Image = ICE_Relative.Properties.Resources.DragAndDrop;
+        }
+
+        void FixTheFiles(BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+
+            List<SpjFiles> spjlist = new List<SpjFiles>();
+            for (int i = 0; i < FileList.Length; i++)
+            {
+                if (FileList[i].Substring(FileList[i].Length - 4) == ".spj")
+                {
+                    spjlist.Add(new SpjFiles { Path = FileList[i] });
+                }
+            }
+
+            int filesCount = spjlist.Count();
+            int processedFilesCount = 0;
+            foreach (var currentFilePath in spjlist) //Files
+            {
+                worker.ReportProgress((int)((float)processedFilesCount / (float)filesCount * 100));
+
+                XmlDocument xml = new XmlDocument();
+                xml.Load(currentFilePath.Path);
+
+                XmlElement root = xml.DocumentElement;
+                XmlNodeList imgs = root.SelectNodes("//stitchProject/sourceImages/sourceImage");
+
+                foreach (XmlNode xmlPicPath in imgs) //Paths
+                {
+                    string xmlImgPath = xmlPicPath.Attributes["filePath"].Value;
+                    xmlPicPath.Attributes["filePath"].Value = ".\\" + xmlImgPath.Substring(xmlImgPath.LastIndexOf('\\'));
+                }
+
+                xml.Save(currentFilePath.Path);
+
+                processedFilesCount++;
+            }
+            worker.ReportProgress((int)((float)processedFilesCount / (float)filesCount * 100));
+        }
+
+        private void DDStart(object sender, DragEventArgs e)
+        {
+            if (processing == false)
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void DDEnd(object sender, DragEventArgs e)
+        {
+            FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            backgroundWorker1.RunWorkerAsync();
+            processing = true;
+
+            e.Effect = DragDropEffects.None;
+            pictureBox1.Image = ICE_Relative.Properties.Resources.DragAndDrop_Process;
+        }
+
+        private void LabelStartDD(object sender, DragEventArgs e)
+        {
+            if (processing == false)
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void LabelEndDD(object sender, DragEventArgs e)
+        {
+            FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            backgroundWorker1.RunWorkerAsync();
+            processing = true;
+
+            e.Effect = DragDropEffects.None;
+            pictureBox1.Image = ICE_Relative.Properties.Resources.DragAndDrop_Process;
+        }
+    }
+
+    public class SpjFiles
+    {
+        public string Path { get; set; }
+    }
+}
